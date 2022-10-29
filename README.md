1ed101fa110e080bd56f708e5481f49b000e9af9
htmujahid <htmujahid@gmail.com>
2022-10-29 20:09:00 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # WebUI
 Web frontend templates.
 
-|Sites  |      |         |
-|-------|------|---------|
-| Salty | [Demo](https://htmujahid.github.io/WebUI/Salty) | [Source Code](https://github.com/htmujahid/WebUI/tree/main/Salty)|
-| Salty | [Demo](https://htmujahid.github.io/WebUI/Salty) | [Source Code](https://github.com/htmujahid/WebUI/tree/main/Salty)|
-| Salty | [Demo](https://htmujahid.github.io/WebUI/Salty) | [Source Code](https://github.com/htmujahid/WebUI/tree/main/Salty)|
+|#  |Sites  |      |         |
+|---|-------|------|---------|
+|0  | Salty | [Demo](https://htmujahid.github.io/WebUI/Salty) | [Source Code](https://github.com/htmujahid/WebUI/tree/main/Salty)|
+|1  | Salty | [Demo](https://htmujahid.github.io/WebUI/Salty) | [Source Code](https://github.com/htmujahid/WebUI/tree/main/Salty)|
+|2  | Salty | [Demo](https://htmujahid.github.io/WebUI/Salty) | [Source Code](https://github.com/htmujahid/WebUI/tree/main/Salty)|
--- Salty/script.js
+++ Salty/script.js
@@ -4,3 +4,36 @@ const navItem1 = document.getElementById('nav-item-1');
 navToggle.addEventListener('click', () => {
     navItem1.classList.toggle('hidden');
 })
+
+const carouselGroup = document.getElementById('carousel-group');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let translation = 0;
+
+if(translation === -2130) {
+    next.disabled = true;
+} else {
+    next.disabled = false;
+}
+
+prev.disabled = true;
+next.addEventListener('click', () => {
+    translation = translation - 426;
+    console.log(translation);
+    carouselGroup.style.transform = `translateX(${translation}px)`;
+    if(translation === -1278) {
+        next.disabled = true;
+    }
+    prev.disabled = false;
+})
+
+prev.addEventListener('click', () => {
+    translation = translation + 426;
+    console.log(translation);
+    carouselGroup.style.transform = `translateX(${translation}px)`;
+    if(translation === 0) {
+        prev.disabled = true;
+    }
+    next.disabled = false;
+})
